34c2ad7e4f1e902199f4944fb99d39fba3e9c8b4
Cnya176 <54885164+Cnya176@users.noreply.github.com>
2020-04-21 15:06:38 +0300

    ¯\_(ツ)_/¯

---
+++ cozerы.py
@@ -0,0 +1,12 @@
+json = {"bomonque": [{"iu": [{"iu-1": []}, {"iu10": [{"cozers": ["Zversky", "GoldGem", "Cnya176"]}]}]},
+                     {"fn": [{"fn-12": [{"obercozers": [], "cozers": ["Ivanov", "Petrov"]}]}]}]}
+
+for bomonqa in json:
+    for group in json[bomonqa]:
+        for iu in group:
+            for kaf in group[iu]:
+                for kaf10 in kaf:
+                    for cors in kaf[kaf10]:
+                        for coders in cors:
+                            for bubuni in cors[coders]:
+                                print(bubuni)
